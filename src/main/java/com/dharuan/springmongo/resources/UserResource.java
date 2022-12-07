8c8501ad5d9095ac11d1e0ac45335628a1d9e41a
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-07 17:24:13 -0300

    Delete user

--- src/main/java/com/dharuan/springmongo/resources/UserResource.java
+++ src/main/java/com/dharuan/springmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,11 @@ public class UserResource {
 		user = service.insert(user);
 		var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
