be6f09aa049189986733ef2908a357e5a363d80a
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-07 14:55:25 -0300

    Database initial seed development

---
+++ src/main/java/com/dharuan/springmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.dharuan.springmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.dharuan.springmongo.domain.User;
+import com.dharuan.springmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		var maria = new User(null, "Maria Brown", "maria@gmail.com");
+		var alex = new User(null, "Alex Green", "alex@gmail.com");
+		var bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
