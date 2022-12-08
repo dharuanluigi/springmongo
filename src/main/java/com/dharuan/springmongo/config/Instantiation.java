e4757c07a783de2677e8cda82dcf28505e63c5dc
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-07 21:38:28 -0300

    Add comment to posts

--- src/main/java/com/dharuan/springmongo/config/Instantiation.java
+++ src/main/java/com/dharuan/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.dharuan.springmongo.domain.Post;
 import com.dharuan.springmongo.domain.User;
 import com.dharuan.springmongo.dto.AuthorDTO;
+import com.dharuan.springmongo.dto.CommentDTO;
 import com.dharuan.springmongo.repository.PostRepository;
 import com.dharuan.springmongo.repository.UserRepository;
 
@@ -39,6 +40,14 @@ public class Instantiation implements CommandLineRunner {
 
 		var post2 = new Post(null, Instant.parse("2018-03-23T14:34:32Z"), "Bom dia!", "Acordei feliz hoje!",
 				new AuthorDTO(maria));
+
+		var comment1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T11:34:32Z"), new AuthorDTO(alex));
+		var comment2 = new CommentDTO("Aproveite", Instant.parse("2018-03-22T15:34:32Z"), new AuthorDTO(bob));
+		var comment3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-23T07:34:32Z"),
+				new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().add(comment3);
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
